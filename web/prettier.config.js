e74d00dc64d771e3ea27a342f723f64ad900fe43
yoshiki-maruya <yoshiki.maruya@gmail.com>
2023-05-02 15:34:51 +0900

    setup prettier

---
+++ web/prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  tailwindConfig: './tailwind.config.js',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "off",
+}
