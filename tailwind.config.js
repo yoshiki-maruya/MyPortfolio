92ec61a66dd23fdf80a162f42ad88a051d744842
yoshiki-maruya <yoshiki.maruya@gmail.com>
2023-05-05 23:23:12 +0900

    change tailwind setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      body: [
+        'Avenir',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'Hiragino Sans',
+        'ヒラギノ角ゴシック',
+        'メイリオ',
+        'Meiryo',
+        'YuGothic',
+        'Yu Gothic',
+        'ＭＳ Ｐゴシック',
+        'MS PGothic',
+        'sans-serif'
+      ]
+    },
     extend: {},
   },
   plugins: [],
