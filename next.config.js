021f56eae1ea8612ba735f842bd6aa461d0f8f59
yoshiki-maruya <yoshiki.maruya@gmail.com>
2023-09-18 01:43:41 +0900

    update next version@13.4

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  env: {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  }
 }
 
 module.exports = nextConfig
