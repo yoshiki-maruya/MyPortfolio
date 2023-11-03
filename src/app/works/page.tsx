074af21abb444deb8b53f750c79a2d492869a5ec
yoshiki-maruya <yoshiki.maruya@gmail.com>
2023-11-03 16:26:20 +0900

    [feature] created works page(draft)

---
+++ src/app/works/page.tsx
@@ -0,0 +1,8 @@
+export default function Works() {
+    return (
+      <div className="mx-auto max-w-xl px-6 py-8">
+        <h1 className="mb-8 text-center text-2xl font-black">Works</h1>
+      </div>
+    )
+  }
+  
