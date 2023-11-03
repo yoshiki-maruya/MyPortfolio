9dd00795453093804fad33df521026cbe45d8ed2
yoshiki-maruya <yoshiki.maruya@gmail.com>
2023-11-04 00:52:23 +0900

    [impl] deleted unnecessary files

--- src/app/blog/head.tsx
+++
@@ -1,6 +0,0 @@
-import { NextSeo } from "next-seo";
-import { SITE } from "../../config";
-
-export default function Head() {
-  return <NextSeo title={SITE.title} description={SITE.description} />;
-}
--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports.SITE = {
-  name: "Yoshiki's Portfolio",
-  title: "Yoshiki's Portfolio",
-  description: 'A website to showcase your work and blog',
-}
