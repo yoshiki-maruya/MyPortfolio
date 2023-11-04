fe387e857b4c0be1478a99c36b5a071f8575163f
yoshiki-maruya <yoshiki.maruya@gmail.com>
2023-11-04 15:34:44 +0900

    [feature] added jest-junit

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,22 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true, // カバレッジ機能の有効化
+  coverageDirectory: 'coverage', // カバレッジの計測結果を配置するディレクトリの指定
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'jest tests',
+        outputDirectory: 'test-results',
+        outputName: 'jest-junit.xml',
+        classNameTemplate: '{classname}-{title}',
+        titleTemplate: '{classname}-{title}',
+        ancestorSeparator: ' › '
+      }
+    ]
+  ]
 };
 
 // createJestConfigを定義することによって、本ファイルで定義された設定がNext.jsの設定に反映されます
