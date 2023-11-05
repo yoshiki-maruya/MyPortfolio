a04cee2ceaa63f941556b047696ec355d695fd24
yoshiki-maruya <yoshiki.maruya@gmail.com>
2023-11-05 20:40:07 +0900

    [impl] exec prettier

--- src/app/api/sendMail/route.ts
+++ src/app/api/sendMail/route.ts
@@ -1,16 +1,16 @@
 import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req: NextRequest) {  
-  if(req.method === 'POST') {
-    const { name, email, message } = await req.json()
-    const sgMail = require('@sendgrid/mail');
+export async function POST(req: NextRequest) {
+  if (req.method === "POST") {
+    const { name, email, message } = await req.json();
+    const sgMail = require("@sendgrid/mail");
     sgMail.setApiKey(process.env.SENDGRID_API_KEY); //SendGridのAPIキー
 
     const msgToManager = {
-      to: 'yoshiki.maruya@gmail.com',
-      from: 'yoshiki.maruya@gmail.com',
-      subject: 'ポートフォリオサイトからの問い合わせ',
-      text: name +'様からお問合せがありました。' + 'メッセージ：' + message + 'アドレス：' + email,
+      to: "yoshiki.maruya@gmail.com",
+      from: "yoshiki.maruya@gmail.com",
+      subject: "ポートフォリオサイトからの問い合わせ",
+      text: name + "様からお問合せがありました。" + "メッセージ：" + message + "アドレス：" + email,
       html: `
         <p>【名前】</p>
         <p>${name}</p>
@@ -20,12 +20,12 @@ export async function POST(req: NextRequest) {
         <p>${message}</p>
       `,
     };
-     
+
     const msgToUser = {
       to: email,
-      from: 'yoshiki.maruya@gmail.com',
-      subject: 'お問合せありがとうございました。',
-      text: 'お問合せを受け付けました。回答をお待ちください。' + message,
+      from: "yoshiki.maruya@gmail.com",
+      subject: "お問合せありがとうございました。",
+      text: "お問合せを受け付けました。回答をお待ちください。" + message,
       html: `
         <p>${name}様</p>
         <p>お問合せを受け付けました。回答をお待ちください。</p><br/>
@@ -34,7 +34,7 @@ export async function POST(req: NextRequest) {
         <p>${message}</p>
       `,
     };
- 
+
     try {
       await sgMail.send(msgToManager);
       await sgMail.send(msgToUser);
