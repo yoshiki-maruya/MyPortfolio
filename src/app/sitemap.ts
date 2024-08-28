ace00b230f890a269e7f8600b6d972c7af9e79aa
yoshiki-maruya <yoshiki.maruya@gmail.com>
2024-08-28 23:09:35 +0900

    [feat] add privacy-policy

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { allPosts } from "contentlayer/generated";
 import blogDates from "@/utils/const/blogDates";
 
 export default async function sitemap() {
-  const baseRoutes = ["", "/about", "/works", "/blog", "/contact"];
+  const baseRoutes = ["", "/about", "/works", "/blog", "/contact", "/privacy-policy"];
   const posts = allPosts.map((post) => ({
     url: `${process.env.WEBSITE_HOST_URL}${post.url}`,
     lastModified: post.date,
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,18 @@
+import { createUrl } from "@/utils/url";
 import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="mx-auto max-w-2xl border-t-2 pt-2 my-5 flex justify-between">
-      <small className="ml-2">&copy; 2023 maruyaki</small>
+      <div className="flex justify-start">
+        <small className="ml-2">&copy; 2023 maruyaki</small>
+        <small className="ml-4">
+          <Link href="/privacy-policy">
+            プライバシーポリシー
+          </Link>
+        </small>
+      </div>
       <div className="flex justify-end gap-4 mr-2">
         <Link href="https://www.linkedin.com/in/%E6%85%B6%E8%B2%B4-%E4%B8%B8%E8%B0%B7-33b130226/" target="_blank">
           <Image src="/linkedin.svg" alt="" width="25" height="25" />
