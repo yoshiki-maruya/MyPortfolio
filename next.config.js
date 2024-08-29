14cb911ac324fb84bea0585e7b3d18f765b3663b
yoshiki-maruya <yoshiki.maruya@gmail.com>
2024-08-29 16:12:07 +0900

    [fix] remove unused github token

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
     WEBSITE_HOST_URL: process.env.WEBSITE_HOST_URL,
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
   }
