e558c10522dbb853ca31328c7913d6e8196d50aa
yoshiki-maruya <yoshiki.maruya@gmail.com>
2025-12-21 23:50:46 +0900

    fix: change the About content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,14 +29,14 @@ export default function About() {
       <AboutCard title="Profile">
         普段は外資系のITコンサル会社で働いているまるやきです。
         <br />
-        社会人3年目、プログラミングと投資が好きです。
+        社会人4年目、プログラミングと投資が好きです。
       </AboutCard>
       <AboutCard title="Hobby">
         ・プログラミング（最近はNext.jsとBaaSに関心があります）
         <br />
         ・ファッション（学生時代は下北通いで古着収集をしていましたが、最近は韓国ブランドとかもウォッチしてます）
         <br />
-        ・ラーメン（二郎系が好み、成蹊前ラーメン・桜台二郎・ひばりヶ丘二郎が好き）
+        ・ラーメン（二郎系が好み、桜台二郎・ひばりヶ丘二郎が好き）
       </AboutCard>
       <AboutCard title="My Tech Stack">
         <div className="relative overflow-x-auto sm:rounded-lg border">
