12a7c08fce2dd5bb47d95a5bfdf03eae266a507a
yoshiki-maruya <yoshiki.maruya@gmail.com>
2025-12-22 11:35:34 +0900

    fix(ci): ci error

--- src/app/api/sendMail/route.ts
+++ src/app/api/sendMail/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse, NextRequest } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   if (req.method === "POST") {
     const { name, email, message } = await req.json();
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
     try {
       // 管理者宛メールを送信
